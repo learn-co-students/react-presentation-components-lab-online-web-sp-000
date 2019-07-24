bfe74d73dde54f3dbee341e2281b1707575039ec
jonmackeyparty <jon@jonmackey.party>
2019-07-24 14:13:17 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={event => console.log('No I\'\m not!!!')} />
   </div>,
   document.getElementById('root')
 );
