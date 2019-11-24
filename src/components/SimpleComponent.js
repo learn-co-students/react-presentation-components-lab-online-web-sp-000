f00eeafabc5d73e10e5b845a80ec14ec27dae05b
Yana <Yana.Loginova@cix.csi.cuny.edu>
2019-11-24 17:57:42 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,17 @@
 // Code SimpleComponent Here
+import React, { Component } from "react";
+
+// the App component should render out the GifListContainer component
+class SimpleComponent extends Component {
+  state = { mood: "happy" };
+
+  handleClick = () => {
+    let moodChange = this.state.mood === "happy" ? "sad" : "happy";
+    this.setState({ mood: moodChange });
+  };
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
+export default SimpleComponent;
