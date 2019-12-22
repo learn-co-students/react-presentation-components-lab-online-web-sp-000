c9bb19f9508c9517215e3eee011eb9502ce32833
github username <github email address>
2019-12-22 09:09:13 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(e) => {
+      let newColor = e.target.style.color === 'blue' ? 'black' : 'blue';
+      e.target.style.color = newColor;
+    }} />
   </div>,
   document.getElementById('root')
 );
