45cab367b1d26f8ff85d8578bf1efd13fff2143b
atribecalledarty <arth3rs0ng@gmail.com>
2020-01-04 19:40:27 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+testFunction = () => {
+  this.innerHTML = 'I am just sad'
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={testFunction} />
   </div>,
   document.getElementById('root')
 );
