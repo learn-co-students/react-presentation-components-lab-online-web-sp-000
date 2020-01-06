c03dcf27766b9d803bd8dccbaa55152082eda8d5
Brett Saxon <brettcsaxon@gmail.com>
2020-01-06 14:35:43 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
