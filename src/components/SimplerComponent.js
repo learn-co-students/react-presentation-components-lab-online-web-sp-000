dbd4e94d3d6f0f957de9b319e5a8d8c3d6d1ebb0
github username <github email address>
2020-05-17 18:05:58 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,7 +1,7 @@
 // Code SimplerComponent Here
 import React, { Component } from 'react';
 
-  //const ComponentName = props => <div onClick={props.handleClick}>I am just happy.</div>
+  
   const ComponentName = props => <div onClick={props.handleClick}>I am just happy.</div>
 
 export default ComponentName;
