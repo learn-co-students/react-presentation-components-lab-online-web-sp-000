506cb9dce52cd11464faf2b37fcb605008d165e5
rnkwilliams <rnkwilliams07@gmail.com>
2020-06-19 16:58:32 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+handleSimpleClick = () => {
+  return 'Heeeyyyyyyy!!!'
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.handleSimpleClick} />
   </div>,
   document.getElementById('root')
 );
