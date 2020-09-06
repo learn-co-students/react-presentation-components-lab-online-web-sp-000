ca6fb91da907387bf3e268d1fb5d9951fe0e9397
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-09-06 16:26:07 +0200

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,11 @@
 // Code SimplerComponent Here
+import React from 'react';
+
+const App = props => {
+
+    return(
+        <div onClick={props.handleClick}>{"I am just happy"}</div>
+    )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+handleClick = (e) => {
+  let newColor = e.target.style.color === "red" ? "green" : "red";
+  e.target.style.color = newColor;
+}
+
 ReactDOM.render(
+
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={this.handleClick} />
   </div>,
   document.getElementById('root')
 );
