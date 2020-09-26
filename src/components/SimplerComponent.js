7995d9e26b34310ce891c7578a8f07f3eb4c617e
github username <github email address>
2020-09-26 16:27:06 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,10 @@
 // Code SimplerComponent Here
+import React from 'react'
+
+const SimplerComponent = props => <div onClick={props.handleClick}>I am just happy.</div>
+
+export default SimplerComponent
+
+//stateless functional component
+//render a div tot he page that contains simple text 
+//receives one prop handleClick 
