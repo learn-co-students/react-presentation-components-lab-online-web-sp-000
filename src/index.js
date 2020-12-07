26056f3fb662c1468b17162b0738105be8cbc050
blumenthaler <blumenthaleralex@gmail.com>
2020-12-07 16:38:49 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={(event) => event.target.innerText = "I have changed you"} />
   </div>,
   document.getElementById('root')
 );
