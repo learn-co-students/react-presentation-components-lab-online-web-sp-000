aa3a93b8e64bc4b88fb7ca0bb64c5a18d0e0f7dd
CoderJay06 <jaycru06@yahoo.com>
2021-03-02 18:33:17 -0500

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,25 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+// import SimplerComponent from './SimplerComponent';
+
+class SimpleComponent extends Component { // declared as class inst of component so it can carry state
+   state = {
+      mood: 'happy'
+   };
+
+   handleClick = event => {
+      event.preventDefault();
+      this.setState(prevState => ({
+         mood: prevState.mood === 'happy' ?
+            'sad' : 'happy'
+      }));
+   }
+ 
+   render() {
+      return (
+         <div onClick={this.handleClick}>{this.state.mood}</div>
+      )
+   }
+}
+
+export default SimpleComponent;
