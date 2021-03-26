4c5fd602ebc94b3666250245b213dcfff8477541
john million <jpmillion@comcast.net>
2021-03-26 11:51:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={e => e.target.innerHTML = "<h1>you did it</h1>"} />
   </div>,
   document.getElementById('root')
 );
