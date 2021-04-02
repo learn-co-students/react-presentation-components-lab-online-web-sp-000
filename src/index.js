b40cc786e3ba695c58db928f45a2db8d6856814d
Catherine Gloria <catherinegloria@gmail.com>
2021-04-02 12:07:00 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={ console.log("Let's do a happy dance!") } />
   </div>,
   document.getElementById('root')
 );
